869c39536e925522f9e3f61624ef7fbe48311da8
LeoZhang <zhangxin1992pm@gmail.com>
2016-11-04 16:03:22 +0800

    improve experiments results visualization.

--- back-propagation/src/main/resources/plotting.py
+++ back-propagation/src/main/resources/plotting.py
@@ -22,11 +22,33 @@ grouping_colors={
     'sg': 'b',
     'kg': 'k'
 }
+
+grouping_sources_colors={
+    0: 'r',
+    1: 'g',
+    2: 'b',
+    3: 'k'
+}
+
 grouping_style={
     'bpg': grouping_colors['bpg'] + '^-',
     'pkg': grouping_colors['pkg'] + 'o--',
     'sg': grouping_colors['sg'] + 'x--',
     'kg': grouping_colors['kg'] + 's--'
+}
+
+grouping_sources_indices={
+    'bpg-5': 0,
+    'pkg-5': 1,
+    'bpg-10': 2,
+    'pkg-10': 3
+}
+
+grouping_sources_labels={
+    'bpg-5': '$bpg_5$',
+    'bpg-10': '$bpg_{10}$',
+    'pkg-5': '$pkg_5$',
+    'pkg-10': '$pkg_{10}$'
 }
 
 def plotImbalance(imbalanceMap, ylabel, xlabel, legendLabels):
@@ -120,8 +142,7 @@ def plotThroughput(throughputContainer, ylabel, xlabel, legendLabels):
         os.mkdir('expViz')
     plt.savefig('expViz/exp3.png')
 
-
-def main():
+def exp3_viz():
     groupingList = []
     metrics_dir = 'exp3'
     throughputContainer = pd.DataFrame()
@@ -140,6 +161,65 @@ def main():
         throughputContainer.loc[str(delay), grouping] = throughput
     plotThroughput(throughputContainer, ylabel='Throughput (keys/s)', xlabel='Worker task delay(ms)', legendLabels=groupingList)
 
+
+def plotImbalanceRobustness(imbalanceContainer, ylabel, xlabel, legendLabels):
+    fig, ax = plt.subplots(1, 1, sharey=True)
+    x = list(imbalanceContainer.index)
+    for ind1, ele in enumerate(x):
+        x[ind1] = int(x[ind1])
+    imbalanceContainer.index = x
+    imbalanceContainer = imbalanceContainer.sort_index()
+    imbalanceContainer = imbalanceContainer.sort_index(axis=1)
+    x = list(imbalanceContainer.index)
+    w = 0.3
+    expand_coe = np.ceil(w * legendLabels.__len__())
+    x_axis = list(expand_coe * np.asarray(range(1, x.__len__() + 1)))
+    cols = list(imbalanceContainer.columns)
+    handles=[]
+    for ind, col in enumerate(cols):
+        handle = ax.bar(np.asarray(x_axis) - (cols.__len__() / 2 - ind) * w, imbalanceContainer.loc[:, col].values, \
+                        width=w, color=grouping_sources_colors[col], align='center', log=True)
+        handles.append(handle)
+    ax.set(xlabel=xlabel)
+    ax.set_yscale('log')
+    ax.set_xlim([0, expand_coe*(x.__len__() + 1)])
+    ax.set_ylim([1.0E-4, 1.0E-1])
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(x)
+    ax.legend(handles, legendLabels, loc='upper left')
+    fig.text(0.04, 0.5, ylabel, va='center', rotation='vertical', size='large')
+    # plt.show()
+    if not os.path.exists('expViz'):
+        os.mkdir('expViz')
+    plt.savefig('expViz/exp2.png')
+    pass
+
+def exp2_viz():
+    groupingSourcesList = []
+    metrics_dir = 'exp2'
+    imbalanceContainer = pd.DataFrame()
+    for metrics_file in os.listdir(metrics_dir):
+        print('processing file: ' + metrics_file)
+        metrics = json.loads(open(metrics_dir + '/' + metrics_file, 'r').read())
+        dataset = str(metrics['dataset'])
+        sources = int(metrics['sources'])
+        workers = int(metrics['workers'])
+        grouping = str(metrics['grouping'])
+        if not groupingSourcesList.__contains__(grouping + '-' + str(sources)):
+            groupingSourcesList.append(grouping + '-' + str(sources))
+        delay = int(metrics['delay'])
+        imbalance = float(metrics['imbalance'])
+        throughput = int(metrics['throughput'])
+        imbalanceContainer.loc[str(workers), grouping_sources_indices[grouping + '-' + str(sources)]] = imbalance
+    groupingSourcesLabels = [None] * groupingSourcesList.__len__()
+    for ele in groupingSourcesList:
+        groupingSourcesLabels[grouping_sources_indices[ele]] = grouping_sources_labels[ele]
+    plotImbalanceRobustness(imbalanceContainer, ylabel='Imbalance Rate', xlabel='workers',
+                           legendLabels=groupingSourcesLabels)
+
+def main():
+    # experiment 4: chronological stable
+    pass
 
 if __name__=='__main__':
     main()
